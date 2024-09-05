97300d9c99d67e024f941b0904c78c79922dbc14
Ahammed Irfan Ali <69192055+irfanpa005@users.noreply.github.com>
2024-09-05 07:54:55 +0530

    first commit

---
+++ students/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Student
+import re
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+    
+    def validate_first_name(self, value):
+        if not re.match("^[a-zA-Z\s\-]+$", value):
+            raise serializers.ValidationError("Name shouldnot have special characters.")
+        return value
+
+    def validate_last_name(self, value):
+        if not re.match("^[a-zA-Z\s\-]+$", value):
+            raise serializers.ValidationError("Name shouldnot have special characters.")
+        return value
+
+    def validate_gpa(self, value):
+        if value < 0.0 or value > 4.0:
+            raise serializers.ValidationError("GPA must be between 0.0 and 4.0")
+        return value
---
+++ students/views.py
@@ -0,0 +1,71 @@
+from django.forms import ValidationError
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Student
+from .serializers import StudentSerializer
+
+# Create your views here.
+
+class StudentsViewSet(viewsets.ModelViewSet):
+    serializer_class = StudentSerializer
+    queryset = Student.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        try:
+            response = super().create(request, *args, **kwargs)
+            return Response({
+                "message": "Student added successfully",
+                "status": status.HTTP_200_OK,
+                "data": response.data
+            }, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            return Response({
+                "message": "Failed to add student",
+                "status": status.HTTP_400_BAD_REQUEST,
+                "errors": e.detail
+            }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({
+                "message": "An error occurred while adding the student",
+                "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                "error": str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+    def partial_update(self, request, *args, **kwargs):
+        try:
+            response = super().partial_update(request, *args, **kwargs)
+            return Response({
+                "message": "Student updated successfully",
+                "status": status.HTTP_200_OK,
+                "data": response.data
+            }, status=status.HTTP_200_OK)
+        except ValidationError as e:
+            return Response({
+                "message": "Failed to update student",
+                "status": status.HTTP_400_BAD_REQUEST,
+                "errors": e.detail
+            }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({
+                "message": "An error occurred while updating the student",
+                "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                "error": str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            # Call the parent class's destroy method
+            super().destroy(request, *args, **kwargs)
+            return Response({
+                "message": "Student removed successfully",
+                "status": status.HTTP_204_NO_CONTENT,
+            }, status=status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            return Response({
+                "message": "An error occurred while removing the student",
+                "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                "error": str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
